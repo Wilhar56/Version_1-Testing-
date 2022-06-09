e80f46037dafaec9deadd0811c3d88a135011d2e
Wilhar56 <wilhar56@kingshigh.school.nz>
2022-06-09 13:24:01 +1200

    Working with if changed

--- Start_of_joystick/sketch_mar25a/sketch_mar25a.ino
+++ Start_of_joystick/sketch_mar25a/sketch_mar25a.ino
@@ -1,3 +1,6 @@
+#include <Servo.h>
+Servo myservo;
+
 int VRx = A1;
 int VRy = A0;
 int SW = 8;
@@ -15,7 +18,12 @@ int SW_state = 0;
 int mapX = 0;
 int mapY = 0;
 
+int pos = 0;
+
 void setup() {
+
+  myservo.attach(13);
+  
   Serial.begin(9600); 
   
   pinMode(motorB1, OUTPUT);
@@ -41,22 +49,25 @@ void loop() {  xPosition = analogRead(VRx);
   Serial.print(" | Button: ");
   Serial.println(SW_state);
 
-  if ( 0 < mapX <-10 ) {
-  Serial.print(" off ");
-  digitalWrite (motorB1,LOW);
-  digitalWrite (motorB2,LOW);
-  digitalWrite (motorA1,LOW);
-  digitalWrite (motorA2,LOW);
-  }
-  else if (mapX >= -400) {
+
+  if (mapX <= -400) {
     Serial.print(" Forward ");
     digitalWrite (motorB1,HIGH);
     digitalWrite (motorB2,LOW);
     digitalWrite (motorA1,HIGH);
     digitalWrite (motorA2,LOW);
   } 
+
+
+  if ((mapX >= -10) && (mapX<= 10)) {
+    Serial.print(" off ");
+    digitalWrite (motorB1,LOW);
+    digitalWrite (motorB2,LOW);
+    digitalWrite (motorA1,LOW);
+    digitalWrite (motorA2,LOW);
+  }
   
-  else if (mapX >= 400) {
+  if (mapX >= 400) {
     Serial.print(" Reverse ");
     digitalWrite (motorB1,LOW);
     digitalWrite (motorB2,HIGH);
@@ -64,11 +75,31 @@ void loop() {  xPosition = analogRead(VRx);
     digitalWrite (motorA2,HIGH);
   } 
 
-//  else if (SW_state = 0) {
-//    Serial.print(" Water On ");
-//  }
-//  else if (SW_state ==1) {
-//    Serial.print(" Water Off ");
-//  } 
+  if (mapY >= 400) {
+    Serial.print(" Sideways Right ");
+    digitalWrite (motorB1,HIGH);
+    digitalWrite (motorB2,HIGH);
+    digitalWrite (motorA1,LOW);
+    digitalWrite (motorA2,LOW);
+  } 
 
+  if (mapY <= -400) {
+    Serial.print(" Sideways Left ");
+    digitalWrite (motorB1,LOW);
+    digitalWrite (motorB2,LOW);
+    digitalWrite (motorA1,HIGH);
+    digitalWrite (motorA2,HIGH);
+  } 
+
+  if (SW_state == 0) {
+    Serial.print(" Water On ");
+    myservo.attach(13);
+    for (pos = 0; pos <= 160; pos += 1) { 
+    myservo.write(pos);   
+  }
+  }
+  if (SW_state == 1) {
+    Serial.print(" Water Off ");
+    myservo.detach();
+}
 }
