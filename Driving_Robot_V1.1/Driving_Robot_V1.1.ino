dbee954236407500b24deaa2e24f37e77ea3aa3f
Wilhar56 <wilhar56@kingshigh.school.nz>
2022-06-29 14:52:33 +1200

    Relay test V1.1

---
+++ Driving_Robot_V1.1/Driving_Robot_V1.1.ino
@@ -0,0 +1,124 @@
+#include <Servo.h>
+Servo myservo;
+
+#define F_Sensor 2 //Flame sensor
+
+int VRx = A1;
+int VRy = A0;
+int SW = 8;
+
+int vSpeed = 1; 
+
+const int motorB1      = 7; 
+const int motorB2      = 6; 
+const int motorA1      = 5; 
+const int motorA2      = 4; 
+
+int xPosition = 0;
+int yPosition = 0;
+int SW_state = 0;
+int mapX = 0;
+int mapY = 0;
+
+int pos = 0;
+
+void setup() {
+
+  myservo.attach(13);
+  
+  Serial.begin(9600); 
+
+  pinMode(F_Sensor, INPUT);
+  
+  pinMode(motorB1, OUTPUT);
+  pinMode(motorB2, OUTPUT);
+  pinMode(motorA1, OUTPUT);
+  pinMode(motorA2, OUTPUT);
+  
+  pinMode(VRx, INPUT);
+  pinMode(VRy, INPUT);
+  pinMode(SW, INPUT_PULLUP); 
+}
+
+void loop() {  
+
+  int fire = digitalRead(F_Sensor);
+  
+  xPosition = analogRead(VRx);
+  yPosition = analogRead(VRy);
+  SW_state = digitalRead(SW);
+  mapX = map(xPosition, 0, 1023, -512, 512);
+  mapY = map(yPosition, 0, 1023, -512, 512);
+  
+  Serial.print("X: ");
+  Serial.print(mapX);
+  Serial.print(" | Y: ");
+  Serial.print(mapY);
+  Serial.print(" | Button: ");
+  Serial.println(SW_state);
+
+
+  if (mapX <= -400) {
+    Serial.print(" Forward ");
+    digitalWrite (motorB1,HIGH);
+    digitalWrite (motorB2,LOW);
+    digitalWrite (motorA1,HIGH);
+    digitalWrite (motorA2,LOW);
+  } 
+
+
+  if ((mapX >= -10) && (mapX<= 10)) {
+    Serial.print(" off ");
+    digitalWrite (motorB1,LOW);
+    digitalWrite (motorB2,LOW);
+    digitalWrite (motorA1,LOW);
+    digitalWrite (motorA2,LOW);
+  }
+  
+  if (mapX >= 400) {
+    Serial.print(" Reverse ");
+    digitalWrite (motorB1,LOW);
+    digitalWrite (motorB2,HIGH);
+    digitalWrite (motorA1,LOW);
+    digitalWrite (motorA2,HIGH);
+  } 
+
+  if (mapY >= 400) {
+    Serial.print(" Sideways Right ");
+    digitalWrite (motorB1,HIGH);
+    digitalWrite (motorB2,HIGH);
+    digitalWrite (motorA1,LOW);
+    digitalWrite (motorA2,LOW);
+  } 
+
+  if (mapY <= -400) {
+    Serial.print(" Sideways Left ");
+    digitalWrite (motorB1,LOW);
+    digitalWrite (motorB2,LOW);
+    digitalWrite (motorA1,HIGH);
+    digitalWrite (motorA2,HIGH);
+  } 
+
+  if (SW_state == 0) {
+    Serial.print(" Water On ");
+    myservo.attach(13);
+    for (pos = 0; pos <= 160; pos += 1) { 
+    myservo.write(pos);   
+  }
+  }
+  if (SW_state == 1) {
+    Serial.print(" Water Off ");
+    myservo.detach();
+}
+
+
+  if( fire == HIGH)
+  {
+    Serial.println("Fire Detected");
+  }
+  
+  if( fire == LOW)
+  {
+    Serial.println("Peace");
+  }
+}
---
+++ Relay_test/Relay_test.ino
@@ -0,0 +1,61 @@
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-button-pump
+ */
+
+#include <ezOutput.h>
+
+ezOutput pump(2);    // create ezOutput object attached to pin 2
+
+void setup() {
+  Serial.begin(9600);
+  pump.low(); // turn pump off
+}
+
+void loop() {
+  pump.loop();   // MUST call the loop() function first
+    Serial.println("Pump is started");
+    pump.low();
+    pump.pulse(4000); // turn on for 10000 milliseconds ~ 10 seconds
+    Serial.println("end");
+    // after 10 seconds, pump will be turned off by pump.loop() function
+}
+
+
+
+
+
+
+
+
+///*
+// * Created by ArduinoGetStarted.com
+// *
+// * This example code is in the public domain
+// *
+// * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-controls-pump
+// */
+//
+//// constants won't change
+//const int RELAY_PIN = A5;  // the Arduino pin, which connects to the IN pin of relay
+//
+//// the setup function runs once when you press reset or power the board
+//void setup() {
+//  Serial.begin(9600);
+//  // initialize digital pin A5 as an output.
+//  pinMode(RELAY_PIN, OUTPUT);
+//}
+//
+//// the loop function runs over and over again forever
+//void loop() {
+//  digitalWrite(RELAY_PIN, HIGH); // turn on pump 5 seconds
+//  Serial.println("ON");
+//  delay(5000);
+//  digitalWrite(RELAY_PIN, LOW);  // turn off pump 5 seconds
+//  Serial.println("ON");
+//
+//  delay(5000);
+//}
